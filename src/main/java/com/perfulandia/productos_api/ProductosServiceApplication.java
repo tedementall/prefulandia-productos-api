1aaeeaf0ecc709567114d455bb61a0bd6e3ea179
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 18:26:53 -0400

    Commit inicial del proyecyo

---
+++ src/main/java/com/perfulandia/productos_api/ProductosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.productos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosServiceApplication.class, args);
+	}
+
+}
