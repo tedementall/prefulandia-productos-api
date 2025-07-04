778d4be7c286f771803569a6c3533faa77e53621
Masek830 <victor.ramos.saldias@gmail.com>
2025-07-03 22:18:08 -0400

    Hateoas

--- src/main/java/com/productos/dto/ProductoDTO.java
+++ src/main/java/com/productos/dto/ProductoDTO.java
@@ -1,11 +1,13 @@
 package com.productos.dto;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductoDTO {
+public class ProductoDTO extends RepresentationModel<ProductoDTO> {
     private Integer id;
     private String nombre;
     private String descripcion;
